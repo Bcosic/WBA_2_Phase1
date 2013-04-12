0ea524490b5029ba36b30724394d2d547b9dbfab
bcosic <Boris.Cosic@gmx.de>
2013-04-12 18:15:36 +0200

    Phase 1

---
+++ Aufgabe 2.js
@@ -0,0 +1,24 @@
+var gruppenleiter= {
+		
+		"first_name": "Hans",
+		"last_name": "Werner",
+		"email": "Hans.werner@gmx.de",
+		/*var wernerBday = {"tag":19, "monat":08, "jahr":1977};
+		"bday":wernerBday.tag+"."+wernerBday.monat+"."+wernerBday.jahr,*/
+		"b_day": "19.08.1977" ,				
+		"erfahrung": "Amateur",
+		"schlagzeug": "vorhanden",
+		"anmerkung": "alles super hier"
+		};
+var mitglied= {
+		
+		"first_name": "Sven",
+		"last_name": "Olm",
+		"email": "Sven.Olm@aol.com",
+		"b_day": "18.03.2003" ,
+		"erfahrung": "Profi",
+		"schlagzeug": "vorhanden"	
+		};
+var agb= {
+		
+}
